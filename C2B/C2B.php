21eb8bda1213454457eec4b423a3d6de88a2d343
clintonngotta <clintonngotta@gmail.com>
2019-06-16 08:23:20 +0300

    modify c2b

--- C2B/C2B.php
+++ C2B/C2B.php
@@ -7,7 +7,7 @@ $registerurl = $c2b->RegisterC2BUrl($accesstoken);
 
 $register_url_response = json_decode($registerurl, true);
 if($register_url_response['ResponseDescription'] == 'success'){
-	$simulate_rquest = $c2b->ResponseDescription($accesstoken);
+	$simulate_rquest = $c2b->SimulateC2B($accesstoken);
 	print_r($simulate_rquest);
 }else{
 	print_r($register_url_response);
